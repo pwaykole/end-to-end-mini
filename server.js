2cbd82db5e2934bb46665e5709d6517d706f4a3d
pwaykole <prat.pattu@gmail.com>
2016-11-03 14:24:23 -0400

    some code

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyparser = require('body-parser');
 var morgan = require('morgan');
+
+var app = express();
+
+app.use(bodyparser.json());
